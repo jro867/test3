f6d0712bfc742a7040f883ab0ebf67558aec42ea
Chuy <jro867@gmail.com>
2014-04-05 20:59:40 -0600

    bs

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: nuxer
+ *
+ * Created on April 5, 2014, 8:51 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout << "pease of crap..";
+    return 0;
+}
+
